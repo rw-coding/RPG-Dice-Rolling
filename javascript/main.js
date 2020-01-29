aa3e20dc20ef15a171b5441db8fe86d9568b4da4
rw-coding <EliteGuardians123456@gmail.com>
2020-01-29 15:21:19 -0600

    Dice images added, random numbers for d4 dice added.

--- javascript/main.js
+++ javascript/main.js
@@ -7,53 +7,62 @@ let d10button = document.getElementById("d10button");
 let d12button = document.getElementById("d12button");
 let d20button = document.getElementById("d20button");
 let current = 1;
+let diceRoll = 0;
 let rollButton = document.getElementById("roll");
 
 /* Dice variables */
 
-let d4 = document.getElementById("d4");
-let d6 = document.getElementById("d6");
-let d8 = document.getElementById("d8");
-let d10 = document.getElementById("d10");
-let d12 = document.getElementById("d12");
-let d20 = document.getElementById("d20");
+let changing = document.getElementById("changing");
 
 /* Button functions */
 
 function d4change() {
     if ( current != 1 ) {
         current = 1;
+        changing.src = "images/d4.png";
     };
 }
 
 function d6change() {
     if ( current != 2 ) {
         current = 2;
+        changing.src = "images/d6.png";
     };
 }
 
 function d8change() {
     if ( current != 3 ) {
         current = 3;
+        changing.src = "images/d8.png";
     };
 }
 
 function d10change() {
     if ( current != 4 ) {
         current = 4;
+        changing.src = "images/d10.png";
     };
 }
 
 function d12change() {
     if ( current != 5 ) {
         current = 5;
+        changing.src = "images/d12.png";
     };
 }
 
 function d20change() {
     if ( current != 6 ) {
         current = 6;
+        changing.src = "images/d20.png";
     };
 }
 
-/* Effects of current */
+/* Dice Functions */
+
+function rollTheDice() {
+    if ( current = 1 ) {
+        diceRoll = Math.floor(Math.random() * 4) + 1;
+        console.log(diceRoll);
+    };
+}
